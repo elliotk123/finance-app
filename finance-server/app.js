1b1b8c5af7118a1018a63e11926a354ab598dbb4
elliotk123 <32294584+elliotk123@users.noreply.github.com>
2019-05-12 00:33:18 +0100

    data now fetched from S3

--- finance-server/app.js
+++ finance-server/app.js
@@ -2,6 +2,16 @@ const express = require('express')
 const app = express()
 const port = 3000
 const file = require('./data/data.json')
+const AWS = require('aws-sdk');
+const awsCreds = require('./rootkey.json')
+
+
+AWS.config.update({
+    accessKeyId: awsCreds.AWSAccessKeyId,
+    secretAccessKey: awsCreds.AWSSecretKey
+  });
+
+var s3 = new AWS.S3();
 
 app.get('/data', (req, res) => {
     res.set({
@@ -10,4 +20,21 @@ app.get('/data', (req, res) => {
     res.json(file);
 })
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.get('/dataOnline', (req, res) => {
+    res.set({
+        'Access-Control-Allow-Origin': '*'
+    })
+    const params = {Bucket: 'www.connors-stag.com', Key: 'data.json'}
+    //s3://www.connors-stag.com/data.json
+    s3.getObject(params, function(err, data) {
+        if (err){
+            console.log("ERROR!")
+            console.log(err, err.stack); // an error occurred
+        } else {
+            res.send(data.Body);        // successful response
+        }
+    });
+    
+})
+
+app.listen(port, () => console.log(`Listening on port ${port}!`))
